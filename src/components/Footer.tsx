982919cd9bdfe8c5fc0dc0aa736db00e7da354c4
tadeu.cesar <tadeu.cesar@worldconnectoficial.com.br>
2024-10-14 16:13:37 -0300

    primeiro commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,22 @@
+import { Box } from "@chakra-ui/react";
+
+const Footer = () => {
+  return (
+    <Box
+      display={"flex"}
+      alignContent={"center"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      fontSize={"3xl"}
+      textAlign={"center"}
+      backgroundColor={"#696969"}
+      color={"white"}
+      height={"7vh"}
+      width={"100%"}
+    >
+      <h1>Footer</h1>
+    </Box>
+  );
+};
+
+export default Footer;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,17 @@
+import { Box } from '@chakra-ui/react'
+
+const NavBar = () => {
+  return (
+    <Box
+    display={'flex'}
+    width={'79%'}
+    backgroundColor={'#696969'}
+    color={'white'}
+    justifyContent={'center'}
+    >
+      <h1>NavBar</h1>
+    </Box>
+  )
+}
+
+export default NavBar

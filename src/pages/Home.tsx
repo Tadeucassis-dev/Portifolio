45e0bb5953b3055a97145e67d553266817d98359
tadeu.cesar <tadeu.cesar@worldconnectoficial.com.br>
2024-10-15 10:00:29 -0300

    feat: initial commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,27 @@
-import { Box, Text } from '@chakra-ui/react'
+import { Box, Text } from "@chakra-ui/react";
 
 const Home = () => {
   return (
     <Box
-    height={'67vh'}
-    width={'100%'}
-    backgroundColor={'#fff'}
-    color={'black'}
-    justifyContent={'center'}
-    alignItems={'center'}
-    fontSize={'3xl'}
-    textAlign={'center'}
-    display={'flex'}
+      height={"67vh"}
+      width={"100%"}
+      backgroundColor={"#fff"}
+      color={"black"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      fontSize={"3xl"}
+      textAlign={"center"}
+      display={"flex"}
     >
-      <Text mr={300}  fontSize={'20px'} fontWeight={'bold'}>
+      <Text mr={300} fontSize={"20px"} fontWeight={"bold"}>
         <h1>Lorem Ipsum</h1>
-      Lorem Ipsum
-"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, <br/> consectetur, consectetur, adipisci velit..."
-"Não há ninguém que ame a dor por si só,<br/> que a busque e queira tê-la, simplesmente por ser dor..."
-
+        Lorem Ipsum "Neque porro quisquam est qui dolorem ipsum quia dolor sit
+        amet, <br /> consectetur, consectetur, adipisci velit..." "Não há
+        ninguém que ame a dor por si só,
+        <br /> que a busque e queira tê-la, simplesmente por ser dor..."
       </Text>
     </Box>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
